43b60fad8307b655b1d74b73701b09f7db6c5ea8
Srivatsa S <srivatsashankaran@gmail.com>
2017-06-05 20:38:54 +0530

    adding the form to our submit page

--- src/Submit.js
+++ src/Submit.js
@@ -13,9 +13,28 @@ class Submit extends React.Component{
 
   render() {
     return(
-      <div>
-        <h2>Submit</h2>
-        <button onClick={this.submitRecipe}>Submit a recipe</button>
+      <div className="row">
+        <div className="col-xs-12 col-sm-12">
+          <h2>Submit</h2>
+            <form>
+              <div className="form-group">
+                <label for="name">Name</label>
+                <input type="text" className="form-control" id="name" placeholder="Enter the name of the recipe" />
+              </div>
+              <div className="form-group">
+                <label for="description">Description</label>
+                <input type="text" className="form-control" id="name" placeholder="Enter a brief description" />
+              </div>
+              <div className="form-inline form-group">
+                <label for="quantity">Quantity</label>
+                <input type="text" className="form-control" id="quantity" placeholder="Quantity" />
+                <label for="ingredient">Ingredients</label>
+                <input type="text" className="form-control" id="quantity" placeholder="Ingredient" />
+                <button type="submit" className="btn btn-info">Add</button>
+              </div>
+              <button type="submit" class="btn btn-default" onClick={this.submitRecipe}>Submit a recipe</button>
+            </form>
+          </div>
       </div>
     );
   }
