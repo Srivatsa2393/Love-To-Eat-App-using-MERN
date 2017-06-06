4eafc75df37a4d62028fb67846bc54ee03ccde91
Srivatsa S <srivatsashankaran@gmail.com>
2017-06-06 11:44:46 +0530

    stored in a database and then displayed recipes in home page

--- src/Home.js
+++ src/Home.js
@@ -1,9 +1,57 @@
 import React, { Component } from 'react';
+import IngredientList from './IngredientList';
+import axios from 'axios';
 
 class Home extends React.Component{
+  constructor(props){
+    super(props);
+    this.state = {
+      recipes: []
+    };
+    this.loadRecipesFromServer = this.loadRecipesFromServer.bind(this);
+  }
+
+  loadRecipesFromServer(e){
+    console.log('loading the recipes from the database');
+    axios.get('/recipes')
+      .then(response => {
+        this.setState({recipes: response.data.recipes});
+      })
+      .catch(function(error){
+        console.log(error);
+      });
+  }
+
+  componentDidMount(){
+    this.loadRecipesFromServer();
+  }
+
+  displayRecipes(){
+    let resultsArray = [];
+
+    this.state.recipes.map((recipe) => {
+      return resultsArray.push(
+        <div key={recipe['_id']} className="col-sm-6 col-md-4">
+          <div className="thumbnail">
+            <div className="caption">
+              <h3>{recipe.name}</h3>
+              <p>{recipe.description}</p>
+              <IngredientList recipe={recipe} />
+            </div>
+          </div>
+        </div>
+      );
+    });
+    return resultsArray;
+  }
   render() {
     return(
-      <h2>Home</h2>
+      <div className="container">
+          <h2>Home</h2>
+          <div className="row">
+            {this.displayRecipes()}
+          </div>
+      </div>
     )
   }
 }
--- src/Submit.js
+++ src/Submit.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import Ingredients from './Ingredients';
 import IngredientList from './IngredientList';
+import request from 'superagent';
+import axios from 'axios';
 
 class Submit extends React.Component{
   constructor(props){
@@ -16,13 +18,20 @@ class Submit extends React.Component{
   }
   submitRecipe(){
     console.log('button clicked');
-    //this.props.history.push('/');
+    this.props.history.push('/');
     console.log(this.name.value, this.description.value);
     let newRecipe = this.state.newRecipe;
     newRecipe.name = this.name.value;
     newRecipe.description = this.description.value;
     this.setState({newRecipe});
+    //console.log(newRecipe);
+
+    axios.post('/recipes', {recipes: this.state.newRecipe})
+    .then(response => {
+      console.log(response.config.data);
+    });
     console.log(newRecipe);
+    this.props.history.push('/');
   }
 
   addIngredient(quantity, ingredient){
@@ -35,34 +44,36 @@ class Submit extends React.Component{
 
   render() {
     return(
-      <div className="row">
-        <div className="col-xs-12 col-sm-12">
-          <h2>Submit</h2>
-            <form>
-              <div className="form-group">
-                <label htmlFor="name">Name</label>
-                <input type="text"
-                    ref={(input) => {this.name = input;}}
+      <div className="container">
+        <div className="row">
+          <div className="col-xs-12 col-sm-12">
+            <h2>Submit</h2>
+              <form>
+                <div className="form-group">
+                  <label htmlFor="name">Name</label>
+                  <input type="text"
+                      ref={(input) => {this.name = input;}}
+                      className="form-control"
+                      id="name"
+                      placeholder="Enter the name of the recipe"
+                    />
+                </div>
+                <div className="form-group">
+                  <label htmlFor="description">Description</label>
+                  <textarea
+                    type="text"
+                    ref={(input) => {this.description = input;}}
                     className="form-control"
                     id="name"
-                    placeholder="Enter the name of the recipe"
-                  />
-              </div>
-              <div className="form-group">
-                <label htmlFor="description">Description</label>
-                <textarea
-                  type="text"
-                  ref={(input) => {this.description = input;}}
-                  className="form-control"
-                  id="name"
-                  placeholder="Enter a brief description">
-                </textarea>
-              </div>
-              <IngredientList recipe={this.state.newRecipe}/>
-              <Ingredients addIngredient={(quantity, ingredient) => {this.addIngredient(quantity, ingredient)}}/>
-              <button type="button" onClick={this.submitRecipe} class="btn btn-default">Submit a recipe</button>
-            </form>
-          </div>
+                    placeholder="Enter a brief description">
+                  </textarea>
+                </div>
+                <IngredientList recipe={this.state.newRecipe}/>
+                <Ingredients addIngredient={(quantity, ingredient) => {this.addIngredient(quantity, ingredient)}}/>
+                <button type="button" onClick={this.submitRecipe} class="btn btn-default">Submit a recipe</button>
+              </form>
+            </div>
+        </div>
       </div>
     );
   }
